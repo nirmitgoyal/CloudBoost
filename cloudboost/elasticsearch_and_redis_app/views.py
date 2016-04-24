07c45946ab413467938cec0d742c3d5c9a8c55a4
Nirmit Goyal <nirmitgoyal.goyal@gmail.com>
2016-04-24 12:31:54 +0530

    added screenshots

--- cloudboost/elasticsearch_and_redis_app/views.py
+++ cloudboost/elasticsearch_and_redis_app/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from datetime import datetime
 from cloudboost.settings import es,rds
 from django.views.decorators.csrf import csrf_exempt
+# import json
 
 
 
@@ -17,16 +18,20 @@ def cloudboost(request):
 def index(request):
     # book = get_object_or_404(Books)
     if request.method == 'GET':
-    	result=rds.get("message") #since I had to index only one item, i.e. message, I only create a key message,
-    								# otherwise I would have used list in Redis
+    	key=request.GET['key']
+    	result=rds.get(key) 
     	return HttpResponse(result)
     elif request.method == 'POST':
     	message = request.POST['message']
-    	res = es.index(index="test-index", doc_type='cloudboost', body={"message": message, "timestamp": datetime.now()})
-    	res1=rds.set("message",message)
+    	res1 = es.index(index="test-index", doc_type='cloudboost', body={"message": message, "timestamp": datetime.now()})
+    	res2=rds.set("message",message)#since I had to index only one item, i.e. message, I only create a key message,
+    								# otherwise I would have used list in Redis    	
+    	# res1=json.dumps(res1, sort_keys=False, indent=4, separators=(',', ': '))
+    	response="\n\n".join([str(res1),str(res2)])
+    	
     	# template = loader.get_template('elasticsearch_and_redis_app/insert.html')
     	# context={}
-    	return HttpResponse(str(res)+str(res1))
+    	return HttpResponse(response)
     	
     else:
     	return HttpResponse("GET/POST required")
